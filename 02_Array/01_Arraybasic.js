5151b1117712a5333a967733fc80e2d82bac1f3d
nileshpatil <144022325+nilesh6021@users.noreply.github.com>
2024-07-18 17:58:42 +0000

    ayyabasic

---
+++ 02_Array/01_Arraybasic.js
@@ -0,0 +1,47 @@
+// //ARRAY
+// const myarray =[0,1,2,3,4,5,6];
+// console.log(myarray);
+// console.log(myarray[0]);
+
+// const mylist =["A","B","C","D","E"];
+// console.log(mylist);
+// console.log(mylist[3]);
+
+
+// const fruits =[]
+// fruits.push("mango","bananna","apple","beetroot");
+// console.log(fruits);
+// console.log(typeof fruits);
+// console.log(fruits.length);
+
+// fruits.pop(); //remove
+// console.log(fruits)
+
+// fruits.push("beetroot");//add
+// console.log(fruits)
+
+// fruits.unshift("papaya");   //add element at first position
+// console.log(fruits)
+
+
+// const element =['Air','Water','Fire']
+// console.log(element.join())//Air,Water,Fire
+// console.log(element.join(''))//AirWaterFire
+// console.log(element.join('_'));//Air_Water_Fire
+
+// console.log(element.includes('water'));
+
+//slice
+// const num =[0,1,2,3,4,5,6,7,8]
+// console.log(num.slice(5))
+// console.log(num.slice(4,6))
+
+const num1 =['A','B','C','D','E','F','G','H','I']
+console.log(num1.slice(0,3))
+// console.log(num1.slice(3))
+// console.log(num1.slice(3,6))
+
+
+const Yearlist =['JAN','FEB','MAR','APR','MAY','JUN','JUL'];
+console.log(Yearlist.splice())
+console.log(Yearlist)
